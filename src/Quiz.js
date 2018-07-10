5a888949950c0f603a430812e364dd737d557a0d
Niels Meersschaert <niels_meersschaert@mckinsey.com>
2018-07-10 15:19:49 -0400

    Display Quiz Questions and Handle Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,13 +9,17 @@ class Quiz extends Component {
     super(props);
     this.state = {quiz_position: 1};
   }
+  showNextQuestion() {
+    this.setState((prevState) => ({
+      quiz_position: prevState.quiz_position + 1
+    }));
+  }
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
     return(
         <div>
-        { isQuizEnd ?  (<QuizEnd />) : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/> }
-
+        { isQuizEnd ?  (<QuizEnd />) : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} /> }
         </div>
       );
   }
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  handleClick(buttonText) {
+    if(buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
+  }
   render() {
     return(
       <main>
@@ -10,8 +15,9 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
-            //quiz question button logic goes here
+          {this.props.quiz_question.answer_options.map((answer, index) =>
+            <QuizQuestionButton key={index} button_text={answer} clickHandler={this.handleClick.bind(this)}/>
+          )}
           </ul>
         </section>
       </main>
